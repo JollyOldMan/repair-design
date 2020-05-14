0a44a9eda7e97d7f77321f4f7131a9b8684edc4f
Fazulov Ildar <jollybatman123@gmail.com>
2020-05-14 23:41:11 +0500

    add: button scroll-down

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function (event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -20,3 +21,42 @@ document.addEventListener("DOMContentLoaded", function (event) {
     })
 
 })
+*/
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    $(document).on('click', function (e) {
+        if (modal.is(e.target))
+            modal.toggleClass('modal--visible');
+    });
+    
+});
+
+function backToTop() {
+    let button = $('.back-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 50) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({scrollTop: 0}, 1000);
+    })
+}
+
+backToTop();
+
